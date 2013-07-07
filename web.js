149de7d1c03b9135ea59a4c7263940840c07eb5a
utengr <utengr1994@gmail.com>
2013-07-07 15:30:48 +0000

    Updated web.js with buffer

--- web.js
+++ web.js
@@ -1,11 +1,16 @@
 var express = require('express');
 
+var fs = require('fs');
+
+var buff = new Buffer(fs.readFileSync('index.html'));
+
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+    
+    response.send(buff.toString());
 });
-
+ 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
