aa2acf6324d5a3f8e093b303e266c0391c5bdf37
utengr <utengr1994@gmail.com>
2013-07-09 00:25:36 +0000

    Started Modification

--- grader.js
+++ grader.js
@@ -32,6 +32,8 @@ var program = require('commander');
 
 var cheerio = require('cheerio');
 
+var rest = require('restler');
+
 var HTMLFILE_DEFAULT = "index.html";
 
 var CHECKSFILE_DEFAULT = "checks.json";
@@ -58,7 +60,8 @@ var loadChecks = function(checksfile) {
 
 
 var checkHtmlFile = function(htmlfile, checksfile) {
-    $ = cheerioHtmlFile(htmlfile);
+   // $ = cheerioHtmlFile(htmlfile);
+    $ = htmlfile; 
     var checks = loadChecks(checksfile).sort();
     var out = {};
     for(var ii in checks) {
@@ -80,10 +83,18 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url_link', 'url link')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
+    if(program.url){
+        rest.get(program.url).on('complete',function(result) {
+            checkJson=checkHtmlFile(result, program.checks);
+        }); 
+    }
+    else {
+        var checkJson = checkHtmlFile(program.file, program.checks);
+    }
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
 } else {
     exports.checkHtmlFile = checkHtmlFile;
-} 
+}
